fe0e6d3baf3393bbf580fb23e8720e04c6b1d75a
isaac <isaac.code980@gmail.com>
2022-12-10 12:27:21 -0600

    7:90 - SAV, status bar

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Our blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
---
+++ notes.md
@@ -0,0 +1,18 @@
+## 7:88
+
+- safeareaview renders content only within the device that is deemed "safe"
+- hey fill your parent, make sure you always fill the parent (flex: 1)
+- fragments allow to render two elements side by side w/o having to wrap in a specific other container
+
+## 7:89
+
+- SAV only for ios
+- status bar from react native is android only
+- status bar from expo is ios only
+- currentHeight android status bar
+
+## 7:90
+
+- cmd + m android : element inspector
+- cmd + control + z ios : inspector
+- performance monitor debug tools
